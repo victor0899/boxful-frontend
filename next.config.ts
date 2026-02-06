d14db9b1976906db645ab6e3057f7b9e5f695407
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2026-02-06 13:34:14 -0600

    chore: suppress Ant Design React 19 compatibility warning
    Configure webpack to ignore the compatibility warning from Ant Design v5 regarding React 19 support. The warning is informational only and does not affect functionality.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      {
+        module: /node_modules\/antd/,
+        message: /antd v5 support React/,
+      },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
